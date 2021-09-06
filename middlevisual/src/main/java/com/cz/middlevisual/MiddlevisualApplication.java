7d791fe39621e81626f6eb46e24e866129a0b00c
cai.min <cai.min@asiainfo-sec.com>
2021-09-06 16:37:25 +0800

    后端基础架构代码上传

---
+++ middlevisual/src/main/java/com/cz/middlevisual/MiddlevisualApplication.java
@@ -0,0 +1,13 @@
+package com.cz.middlevisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlevisualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlevisualApplication.class, args);
+    }
+
+}
